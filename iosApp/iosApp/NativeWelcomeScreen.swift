3ec9ab95faff912cf94c5675b34e7a90a3a8cf5f
Jakub Trnavsky <jakub.trnavsky@strv.com>
2025-12-09 20:15:38 +0100

    Add more dependencies and SharedViewModel foundations

---
+++ iosApp/iosApp/NativeWelcomeScreen.swift
@@ -0,0 +1,20 @@
+
+import SwiftUI
+import ComposeApp
+
+struct NativeWelcomeScreen: View {
+    var body: some View {
+        NavigationView {
+            VStack {
+                Text("Go to KMP Compose Screen")
+                    .font(.headline)
+                    .padding()
+                    .background(Color.blue)
+                    .foregroundColor(.white)
+                    .cornerRadius(10)
+
+            }
+            .navigationTitle("Native Welcome")
+        }
+    }
+}
